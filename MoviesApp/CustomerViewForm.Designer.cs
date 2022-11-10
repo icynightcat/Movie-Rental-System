e4264b2c8acfebcfd4448f0e777b0a8ccc1cd2ad
icynightcat <117472824+icynightcat@users.noreply.github.com>
2022-11-10 13:49:49 -0700

    test

--- MoviesApp/CustomerViewForm.Designer.cs
+++ MoviesApp/CustomerViewForm.Designer.cs
@@ -28,22 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(523, 120);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // CustomerViewForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 561);
-            this.MaximumSize = new System.Drawing.Size(1000, 600);
-            this.MinimumSize = new System.Drawing.Size(1000, 600);
+            this.ClientSize = new System.Drawing.Size(1122, 737);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximumSize = new System.Drawing.Size(1140, 784);
+            this.MinimumSize = new System.Drawing.Size(1140, 784);
             this.Name = "CustomerViewForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Customer View";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private TextBox textBox1;
     }
 }
--- MoviesApp/CustomerViewForm.cs
+++ MoviesApp/CustomerViewForm.cs
@@ -16,5 +16,10 @@ namespace MoviesApp
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
