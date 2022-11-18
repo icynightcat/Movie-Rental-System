5e73e1dd81c21b131ddb1b4a2aca43421a5a0782
Samuel Brownlee <stbrownlee@shaw.ca>
2022-11-18 11:42:52 -0700

    added datasets, 3 more coming

---
+++ MoviesApp/SQL/SQL datasets/Plans.sql
@@ -0,0 +1,4 @@
+insert into Plans (plan_number, max_concurrent, max_monthly, cost) values (1, 1, 2, 10);
+insert into Plans (plan_number, max_concurrent, max_monthly, cost) values (2, 1, 0, 15);
+insert into Plans (plan_number, max_concurrent, max_monthly, cost) values (3, 2, 0, 20);
+insert into Plans (plan_number, max_concurrent, max_monthly, cost) values (4, 3, 0, 25);
