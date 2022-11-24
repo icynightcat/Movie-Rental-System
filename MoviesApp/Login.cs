dbe6a020f8014e2ae888245564f2fa8aebfaade4
Samuel Brownlee <stbrownlee@shaw.ca>
2022-11-24 13:50:18 -0700

    can now log in to an account of your choice

--- MoviesApp/Login.cs
+++ MoviesApp/Login.cs
@@ -27,37 +27,46 @@ namespace MoviesApp
 
         private void loginButton_Click(object sender, EventArgs e)
         {
-            
-            while(true)
-            {
-                if (accountTextInput.Text.StartsWith("E"))
-                {
-                    string account_ID = accountTextInput.Text.Substring(1);
-                    string query = $"select employee_id from Employees where employee_id = {account_ID}";
-                    SqlDataReader? empdata = connection.GetDataReader(query); //get data reader must be initalized like line 9
 
-                    if (empdata != null && empdata.HasRows)
-                    {
-                        empdata.Read();
-                        string ID = empdata["employee_id"].ToString();      //passing ID
-                        empdata.Close();                //closes the reader after the data is read in
-                        connection.CloseConnection();//closes the database connection not the DBconnection.cs file
-                        this.Close();
-                        new EmployeeViewForm().Show();
-                        break;
-                    }
-                    
-                    //Console.WriteLine(empdata["employee_id"].ToString()); //call to the table, [col name]. ToString so its reable, can be looped with while for the empdata
-                   
-                }
-                else
+            if (accountTextInput.Text.StartsWith("E"))
+            {
+                string account_ID = accountTextInput.Text.Substring(1);
+                string query = $"select employee_id from Employees where employee_id = {account_ID}";
+                SqlDataReader? empdata = connection.GetDataReader(query); //get data reader must be initalized like line 9
+
+                if (empdata != null && empdata.HasRows)
                 {
+                    empdata.Read();                                     //to see the data you must read() first
+                    string ID = empdata["employee_id"].ToString();      //passing ID
+                    empdata.Close();                //closes the reader after the data is read in
                     connection.CloseConnection();//closes the database connection not the DBconnection.cs file
                     this.Close();
+                    new EmployeeViewForm().Show();
 
-                    new CustomerViewForm().Show();
                 }
-                }
+
+                //Console.WriteLine(empdata["employee_id"].ToString()); //call to the table, [col name]. ToString so its reable, can be looped with while for the empdata
+
             }
-        }
+            else
+            {
+                string account_ID = accountTextInput.Text;
+                string query = $"select account_number from Customer where account_number = {account_ID}";
+                SqlDataReader? custdata = connection.GetDataReader(query); //get data reader must be initalized like line 9
+
+                if (custdata != null && custdata.HasRows) //checks if there is data in the table as well as a table
+                {
+                    custdata.Read();                                     //to see the data you must read() first
+                    string ID = custdata["account_number"].ToString();      //passing ID
+                    custdata.Close();                //closes the reader after the data is read in
+                    connection.CloseConnection();//closes the database connection not the DBconnection.cs file
+                    this.Close();
+                    new CustomerViewForm().Show();
+
+                }
+
+            }
+        } 
+            
+    }
 }
