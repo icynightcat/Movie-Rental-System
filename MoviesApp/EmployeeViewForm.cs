e58197f30ec3387a9b2a0317e76879107a1ec442
Jonathan Snook <snookj2@mymacewan.ca>
2022-11-25 12:37:11 -0700

    Makes stuff work

--- MoviesApp/EmployeeViewForm.cs
+++ MoviesApp/EmployeeViewForm.cs
@@ -10,10 +10,34 @@ namespace MoviesApp
         private DBConnection connection;
         public EmployeeViewForm(string input, DBConnection input_connection) //takes the id in
         {
-
             InitializeComponent(); //create the forms
             connection = input_connection;
             ID = input; //call this ID to get the current id, needs to be sent to others as well when tranfering
+        }
+
+        //dataGridView2
+        private void hideSearchElements()
+        {
+            movieNameTextBox.Visible = false;
+            genreComboBox.Visible = false;
+            actorsNamesTextBox.Visible = false;
+            fromYearTextBox.Visible = false;
+            toYearTextBox.Visible = false;
+            moviesSearchButton.Visible = false;
+        }
+        private void showSearchElements()
+        {
+            movieNameTextBox.Visible = true;
+            genreComboBox.Visible = true;
+            actorsNamesTextBox.Visible = true;
+            fromYearTextBox.Visible = true;
+            toYearTextBox.Visible = true;
+            moviesSearchButton.Visible = true;
+        }
+
+        private void searchAll(Object sender, EventArgs e)
+        {
+            hideSearchElements();
 
             string query = $"select * from movie order by movie_name";
             SqlDataReader? empdata = connection.GetDataReader(query);
@@ -24,7 +48,13 @@ namespace MoviesApp
             }
 
             empdata.Close();                //closes the reader after the data is read in
-            
+
+        }
+
+        private void newSearchFilters(Object sender, EventArgs e)
+        {
+            showSearchElements();
+            dataGridView2.Rows.Clear();
         }
 
         private void mostActivelyRented(object sender, EventArgs e)
@@ -35,7 +65,7 @@ namespace MoviesApp
 
         private void launchMovieButton_Click(object sender, EventArgs e)
         {
-                new MovieForm().ShowDialog();
+            new MovieForm().ShowDialog();
         }
         private void launchActorButton_Click(object sender, EventArgs e)
         {
